a1ce60c346279e1922ca35ee7a9ccf00f04f99b7
Aram <aramgrig@hotmail.com>
2017-10-03 21:46:07 +0300

    change config file

--- backend/config/overrides/base.php
+++ backend/config/overrides/base.php
@@ -43,6 +43,7 @@ return [
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                'site/login' => 'site/login',
                // '<controller:\w+>/'=>'<controller>/admin'
 
                 // Your other rules here...
